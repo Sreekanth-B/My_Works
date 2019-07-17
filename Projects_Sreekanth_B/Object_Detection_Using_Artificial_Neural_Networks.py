b400acde57cc9042997b2d45ec5eb777d1379f63
Sreekanth_B <49754899+Sreekanth-B@users.noreply.github.com>
2019-07-17 12:52:34 +0530

    Create Object_Detection_Using_Artificial_Neural_Networks.py

---
+++ Projects_Sreekanth_B/Object_Detection_Using_Artificial_Neural_Networks.py
@@ -0,0 +1,4 @@
+# Created by Sreekanth_B
+# Developing the Object Detection using Using Artificial Neural Networks and Deep learning concepts 
+
+# using open source databases (Keras) for large data training 
