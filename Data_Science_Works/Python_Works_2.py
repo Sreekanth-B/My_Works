5877930dd8c44144c0a542c467aa67b31b174309
Sreekanth_B <49754899+Sreekanth-B@users.noreply.github.com>
2019-07-17 18:23:36 +0530

    Update Python_Works_2.py

--- Data_Science_Works/Python_Works_2.py
+++ Data_Science_Works/Python_Works_2.py
@@ -13,6 +13,20 @@ import numpy as np
 from pathlib import Path
 import pandas as pd
 
+# function
+def parquets_to_df(parquet_folders, col_names):
+    parquet_files = []
+    for path_to_parquet in parquet_folders:
+        files = os.listdir(path_to_parquet)
+        for file in files:
+            if ".parquet" in file:
+                table = pq.read_table(path_to_parquet + file, columns=col_names).to_pandas()
+                parquet_files.append(table)
+    df = pd.concat(parquet_files).reset_index(drop=True)
+
+    return df
+
+# or
 data_dir = Path('/home/qx816/notebooks/ap_25/data/model_input/quality_b_features/')
 full_df = pd.concat(1 b b                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     b
     pd.read_parquet(parquet_file)
